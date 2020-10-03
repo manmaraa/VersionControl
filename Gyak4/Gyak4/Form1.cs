b9046817b40d87b2d498cb56505fac0fcc5e69f0
Matin Manhertz <manhertzmartin@outlook.hu>
2020-10-03 17:40:59 +0200

    Feladat

--- Gyak4/Gyak4/Form1.cs
+++ Gyak4/Gyak4/Form1.cs
@@ -89,6 +89,9 @@ namespace Gyak4
                 values[counter, 8] = "";
                 counter++;
             }
+            xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
         }
         private string GetCell(int x, int y)
         {
