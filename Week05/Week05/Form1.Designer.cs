34b36375068388e76fb71b85ce24c462c63e49ed
Matin Manhertz <manhertzmartin@outlook.hu>
2020-10-08 17:32:54 +0200

    Feladatok(elfelejtettem commitolni néhányszor)

--- Week05/Week05/Form1.Designer.cs
+++ Week05/Week05/Form1.Designer.cs
@@ -29,12 +29,168 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.portfolioDataSet = new Week05.PortfolioDataSet();
+            this.tickBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tickTableAdapter = new Week05.PortfolioDataSetTableAdapters.TickTableAdapter();
+            this.tickidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.indexDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tradingDayDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.priceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.volumeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.portfolioItemBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.indexDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.volumeDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.portfolioDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tickBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.portfolioItemBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.tickidDataGridViewTextBoxColumn,
+            this.indexDataGridViewTextBoxColumn,
+            this.tradingDayDataGridViewTextBoxColumn,
+            this.priceDataGridViewTextBoxColumn,
+            this.volumeDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tickBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(688, 385);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // portfolioDataSet
+            // 
+            this.portfolioDataSet.DataSetName = "PortfolioDataSet";
+            this.portfolioDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tickBindingSource
+            // 
+            this.tickBindingSource.DataMember = "Tick";
+            this.tickBindingSource.DataSource = this.portfolioDataSet;
+            // 
+            // tickTableAdapter
+            // 
+            this.tickTableAdapter.ClearBeforeFill = true;
+            // 
+            // tickidDataGridViewTextBoxColumn
+            // 
+            this.tickidDataGridViewTextBoxColumn.DataPropertyName = "Tick_id";
+            this.tickidDataGridViewTextBoxColumn.HeaderText = "Tick_id";
+            this.tickidDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.tickidDataGridViewTextBoxColumn.Name = "tickidDataGridViewTextBoxColumn";
+            this.tickidDataGridViewTextBoxColumn.ReadOnly = true;
+            this.tickidDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // indexDataGridViewTextBoxColumn
+            // 
+            this.indexDataGridViewTextBoxColumn.DataPropertyName = "Index";
+            this.indexDataGridViewTextBoxColumn.HeaderText = "Index";
+            this.indexDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.indexDataGridViewTextBoxColumn.Name = "indexDataGridViewTextBoxColumn";
+            this.indexDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // tradingDayDataGridViewTextBoxColumn
+            // 
+            this.tradingDayDataGridViewTextBoxColumn.DataPropertyName = "TradingDay";
+            this.tradingDayDataGridViewTextBoxColumn.HeaderText = "TradingDay";
+            this.tradingDayDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.tradingDayDataGridViewTextBoxColumn.Name = "tradingDayDataGridViewTextBoxColumn";
+            this.tradingDayDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // priceDataGridViewTextBoxColumn
+            // 
+            this.priceDataGridViewTextBoxColumn.DataPropertyName = "Price";
+            this.priceDataGridViewTextBoxColumn.HeaderText = "Price";
+            this.priceDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.priceDataGridViewTextBoxColumn.Name = "priceDataGridViewTextBoxColumn";
+            this.priceDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // volumeDataGridViewTextBoxColumn
+            // 
+            this.volumeDataGridViewTextBoxColumn.DataPropertyName = "Volume";
+            this.volumeDataGridViewTextBoxColumn.HeaderText = "Volume";
+            this.volumeDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.volumeDataGridViewTextBoxColumn.Name = "volumeDataGridViewTextBoxColumn";
+            this.volumeDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoGenerateColumns = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.indexDataGridViewTextBoxColumn1,
+            this.volumeDataGridViewTextBoxColumn1});
+            this.dataGridView2.DataSource = this.portfolioItemBindingSource;
+            this.dataGridView2.Location = new System.Drawing.Point(706, 2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(590, 410);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // portfolioItemBindingSource
+            // 
+            this.portfolioItemBindingSource.DataSource = typeof(Week05.Entities.PortfolioItem);
+            // 
+            // indexDataGridViewTextBoxColumn1
+            // 
+            this.indexDataGridViewTextBoxColumn1.DataPropertyName = "Index";
+            this.indexDataGridViewTextBoxColumn1.HeaderText = "Index";
+            this.indexDataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.indexDataGridViewTextBoxColumn1.Name = "indexDataGridViewTextBoxColumn1";
+            this.indexDataGridViewTextBoxColumn1.Width = 125;
+            // 
+            // volumeDataGridViewTextBoxColumn1
+            // 
+            this.volumeDataGridViewTextBoxColumn1.DataPropertyName = "Volume";
+            this.volumeDataGridViewTextBoxColumn1.HeaderText = "Volume";
+            this.volumeDataGridViewTextBoxColumn1.MinimumWidth = 6;
+            this.volumeDataGridViewTextBoxColumn1.Name = "volumeDataGridViewTextBoxColumn1";
+            this.volumeDataGridViewTextBoxColumn1.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1326, 565);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.portfolioDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tickBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.portfolioItemBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private PortfolioDataSet portfolioDataSet;
+        private System.Windows.Forms.BindingSource tickBindingSource;
+        private PortfolioDataSetTableAdapters.TickTableAdapter tickTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tickidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn indexDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tradingDayDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn priceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn volumeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn indexDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn volumeDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.BindingSource portfolioItemBindingSource;
     }
 }
 
