83f11d2c12b75b14309bf76622186098ae8fa17e
Matin Manhertz <manhertzmartin@outlook.hu>
2020-10-15 14:21:40 +0200

    3

--- Week06/Week06/Form1.cs
+++ Week06/Week06/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week06.MnbServiceReference;
 
 namespace Week06
 {
@@ -15,6 +16,16 @@ namespace Week06
         public Form1()
         {
             InitializeComponent();
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+                
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
