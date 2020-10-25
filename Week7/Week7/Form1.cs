7e4004318c63633e68836f6479e7f7b5818a3217
Matin Manhertz <manhertzmartin@outlook.hu>
2020-10-25 19:11:45 +0100

    n

--- Week7/Week7/Form1.cs
+++ Week7/Week7/Form1.cs
@@ -27,6 +27,23 @@ namespace Week7
             Population = GetPopulation(@"C:\Temp\nép.csv");
             BirthProbabilities = GetBirthProb(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProb(@"C:\Temp\halál.csv");
+            for (int year = 2005; year <= 2024; year++)
+            {
+               
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
         public List<Person> GetPopulation(string csvpath)
         {
