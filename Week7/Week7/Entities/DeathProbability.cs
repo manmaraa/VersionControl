44186f0d98c7af602675dd6c357feec4bf45588e
Matin Manhertz <manhertzmartin@outlook.hu>
2020-10-25 18:42:43 +0100

    das

---
+++ Week7/Week7/Entities/DeathProbability.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7.Entities
+{
+    public class DeathProbability
+    {
+        public int YearsOld { get; set; }
+        
+        public int NbrOfChildren { get; set; }
+        public double DeathProb { get; set; }
+
+        
+    }
+}
