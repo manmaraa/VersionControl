dab5892b7ead4a918b9439c820c478e3f0346f53
Matin Manhertz <manhertzmartin@outlook.hu>
2020-10-25 19:21:50 +0100

    vbhj

--- Week7/Week7/Form1.cs
+++ Week7/Week7/Form1.cs
@@ -32,7 +32,7 @@ namespace Week7
                
                 for (int i = 0; i < Population.Count; i++)
                 {
-                    // Ide jön a szimulációs lépés
+                    SimStep(year,Population[i]);
                 }
 
                 int nbrOfMales = (from x in Population
@@ -106,6 +106,34 @@ namespace Week7
             }
 
             return birth;
+        }
+        private void SimStep(int year, Person person)
+        {
+     
+            if (!person.IsAlive) return;
+
+             byte age = (byte)(year - person.BirthYear);
+
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.YearsOld == age
+                             select x.DeathProb).FirstOrDefault();
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+             if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double pBirth = (from x in BirthProbabilities
+                                 where x.YearsOld == age
+                                 select x.BirthProb).FirstOrDefault();
+                 if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
         }
 
     }
