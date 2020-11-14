3b09a9fbfd46a6f8d03fffd87637c481f72b285b
Matin Manhertz <manhertzmartin@outlook.hu>
2020-11-14 17:42:01 +0100

    F

--- Gyak8/Gyak8/Entities/CarFactory.cs
+++ Gyak8/Gyak8/Entities/CarFactory.cs
@@ -7,12 +7,11 @@ using System.Threading.Tasks;
 
 namespace Gyak8.Entities
 {
-   public class BallFactory:IToyFactory
+    public class CarFactory:IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
-       
     }
 }
