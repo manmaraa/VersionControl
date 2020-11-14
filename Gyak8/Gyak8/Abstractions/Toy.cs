068e70c814edd4f504a070169ed8caf5ef19d670
Matin Manhertz <manhertzmartin@outlook.hu>
2020-11-14 17:04:37 +0100

    hjk

--- Gyak8/Gyak8/Abstractions/Toy.cs
+++ Gyak8/Gyak8/Abstractions/Toy.cs
@@ -4,13 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gyak8.Entities
+namespace Gyak8.Abstractions
 {
-    class BallFactory
+    class Toy
     {
-        public Ball CreateNew()
-        {
-            return new Ball();
-        }
     }
 }
