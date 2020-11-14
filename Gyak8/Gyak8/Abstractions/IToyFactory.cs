b7f122acf10b36bc6ff78b5e20c7bf1ca6b7cbde
Matin Manhertz <manhertzmartin@outlook.hu>
2020-11-14 17:35:03 +0100

    fghj

---
+++ Gyak8/Gyak8/Abstractions/IToyFactory.cs
@@ -0,0 +1,14 @@
+﻿using Gyak8.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyak8.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
